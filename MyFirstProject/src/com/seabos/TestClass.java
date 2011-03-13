4b6cb12f00d29f041339acb4ec1592e4b79befb6
SeaGit <Sea@SeaBos.com>
2011-03-12 19:53:07 -0800

    Adding new java project

---
+++ MyFirstProject/src/com/seabos/TestClass.java
@@ -0,0 +1,12 @@
+package com.seabos;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("VP ko gusaa kyun aata hain :)");
+	}
+
+}
