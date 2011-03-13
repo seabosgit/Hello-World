de490769e0f5c942a0efaa23ce0a4fdf2ad0f658
BosGit <VP@.(none)>
2011-03-12 22:59:33 -0500

    //VP is right

--- MyFirstProject/src/com/seabos/TestClass.java
+++ MyFirstProject/src/com/seabos/TestClass.java
@@ -6,7 +6,7 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("VP ko gusaa kyun aata hain :)");
+		System.out.println("VP nahi surya ko gusaa kyun aata hain :)");
 	}
 
 }
